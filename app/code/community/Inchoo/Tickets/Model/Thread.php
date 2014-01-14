0003f8dec591f00fffca255718af3ae00362f106
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2014-01-14 09:59:17 +0100

    list tickets on my account pages

---
+++ app/code/community/Inchoo/Tickets/Model/Thread.php
@@ -0,0 +1,9 @@
+<?php
+
+class Inchoo_Tickets_Model_Thread extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('tickets/thread');
+    }
+}
--- app/code/community/Inchoo/Tickets/controllers/TicketsController.php
+++ app/code/community/Inchoo/Tickets/controllers/TicketsController.php
@@ -4,6 +4,23 @@ class Inchoo_Tickets_TicketsController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        
+        /** get store and customer id */
+        $store_id = Mage::app()->getStore()->getId();
+        $customer_data = Mage::getSingleton('customer/session')->getCustomer();
+        $customer_id = $customer_data->getId();
+
+        /** retreive collection */
+        $tickets = Mage::getModel('tickets/thread')->getCollection()
+            ->addFieldToFilter('customer_id_fk', $customer_id)
+            ->addFieldToFilter('store_id_fk', $store_id);
+
+        /** load layout */
+        Mage::register('tickets_thread', $tickets);
+
+        $this->loadLayout();
+        $this->getLayout()->getBlock('content')->append(
+            $this->getLayout()->createBlock('customer/account_dashboard')
+        );
+        $this->renderLayout();
     }
 }
