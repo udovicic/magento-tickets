a6788f27521cbd539c14a66b918f8785dcff7360
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2014-01-14 11:53:17 +0100

    styled output of messages and threads

--- app/code/community/Inchoo/Tickets/controllers/TicketsController.php
+++ app/code/community/Inchoo/Tickets/controllers/TicketsController.php
@@ -23,4 +23,22 @@ class Inchoo_Tickets_TicketsController extends Mage_Core_Controller_Front_Action
         );
         $this->renderLayout();
     }
+
+    public function threadAction()
+    {
+        $thread_id = $this->getRequest()->getParam('id');
+
+        $posts = Mage::getModel('tickets/post')->getcollection()
+            ->addFieldtoFilter('thread_id_fk', $thread_id);
+        $thread_title = Mage::getModel('tickets/thread')->load($thread_id)->getSubject();
+
+        Mage::register('tickets_post', $posts);
+        Mage::register('tickets_title', $thread_title);
+
+        $this->loadLayout();
+        $this->getLayout()->getBlock('content')->append(
+            $this->getLayout()->createBlock('customer/account_dashboard')
+        );
+        $this->renderLayout();
+    }
 }
