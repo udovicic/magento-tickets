6bb1e99745a761c55837d390ccbf966875dda549
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2014-01-16 15:08:55 +0100

    ajaxed tabs

--- app/code/community/Inchoo/Tickets/Block/Adminhtml/Tabs.php
+++ app/code/community/Inchoo/Tickets/Block/Adminhtml/Tabs.php
@@ -18,10 +18,12 @@ class Inchoo_Tickets_Block_Adminhtml_Tabs extends Mage_Adminhtml_Block_Widget_Ta
             'active' => true,
         ));
 
+        /** ajaxed url */
         $this->addTab('closed_tickets', array(
             'label' => Mage::helper('inchoo_tickets')->__('Closed tickets'),
             'title' => Mage::helper('inchoo_tickets')->__('Closed tickets'),
-            'content' => $this->getLayout()->createBlock('inchoo_tickets/adminhtml_closed')->toHtml(),
+            'class' => 'ajax',
+            'url' => $this->getUrl('*/*/closedgrid', array('_current' => true)),
             'active' => false,
         ));
 
